de06e14f4b6d0159ba625a0eabef3cd16458abbc
TehJesper <54664403+TehJesper@users.noreply.github.com>
2020-01-08 13:57:05 +0100

    Add files via upload

--- TehJesper.github.io-master/ts-skeleton-app-master/build/app.js
+++ TehJesper.github.io-master/ts-skeleton-app-master/build/app.js
@@ -16,61 +16,40 @@ class KeyboardListener {
 }
 KeyboardListener.KEY_ESC = 27;
 KeyboardListener.KEY_SPACE = 32;
-KeyboardListener.ARROW_LEFT = 37;
-KeyboardListener.KEY_LEFT = 65;
-KeyboardListener.ARROW_UP = 38;
-KeyboardListener.KEY_UP = 87;
-KeyboardListener.ARROW_RIGHT = 39;
-KeyboardListener.KEY_RIGHT = 68;
-KeyboardListener.ARROW_DOWN = 40;
-KeyboardListener.KEY_DOWN = 83;
+KeyboardListener.KEY_LEFT = 37;
+KeyboardListener.KEY_UP = 38;
+KeyboardListener.KEY_RIGHT = 39;
+KeyboardListener.KEY_DOWN = 40;
 KeyboardListener.KEY_S = 83;
 class LevelScreen {
     constructor(canvas, ctx, keyboardListener) {
         this.canvas = canvas;
         this.ctx = ctx;
         this.keyboardListener = keyboardListener;
-        this.player = new Player(30, 30, 5, 5, this.keyboardListener);
+        this.player = new Player(290, 0, 3, 3, this.keyboardListener);
     }
     draw() {
-        this.player.move(this.canvas);
+        this.player.move(this.canvas, this.ctx);
         this.player.draw(this.ctx);
     }
 }
 class Maze {
     constructor(canvasId) {
         this.canvas = canvasId;
-        this.canvas.width = 482;
-        this.canvas.height = 482;
+        this.canvas.width = 702;
+        this.canvas.height = 702;
         this.ctx = this.canvas.getContext('2d');
         this.keyboardListener = new KeyboardListener();
         this.levelScreen = new LevelScreen(this.canvas, this.ctx, this.keyboardListener);
-        this.collision = 0;
-        this.x = 200;
-        this.y = 5;
-        this.loadmaze();
         this.loop();
     }
     loop() {
-        requestAnimationFrame(this.loop.bind(this));
-        this.clear();
-        this.levelScreen.draw();
-    }
-    clear() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.loadmaze();
-    }
-    checkCollision() {
-        let imgd = this.ctx.getImageData(this.x, this.y, 15, 15);
-        let pix = imgd.data;
-        for (let i = 0; i < pix.length; i += 4) {
-            if (pix[i] == 0) {
-                this.collision = 1;
-            }
-        }
+        this.levelScreen.draw();
+        requestAnimationFrame(this.loop.bind(this));
     }
     loadmaze() {
-        this.loadImage('maze.gif', this.drawmaze);
+        this.loadImage('goede_maze.jpg', this.drawmaze);
     }
     drawmaze(img) {
         this.ctx.drawImage(img, 0, 0);
@@ -84,7 +63,7 @@ class Maze {
     }
 }
 let init = () => {
-    const Asteroids = new Maze(document.getElementById("canvas"));
+    const maze = new Maze(document.getElementById("canvas"));
 };
 window.addEventListener("load", init);
 class Player {
@@ -94,34 +73,46 @@ class Player {
         this.xVel = xVel;
         this.yVel = yVel;
         this.keyboardListener = keyboardListener;
+        this.collision = 0;
+        this.coin = [];
+        this.coin.push();
+        for (let i = 0; i < 5; i++) {
+            this.coin.push(this.rand(2, 645, 22), this.rand(2, 645, 22));
+        }
+        this.coinCounter = 0;
     }
-    move(canvas) {
-        if (this.keyboardListener.isKeyDown(KeyboardListener.ARROW_RIGHT) && this.xPos !== 482) {
+    move(canvas, ctx) {
+        if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_RIGHT) && this.xPos !== 702) {
             this.xPos += this.xVel;
+            this.checkCollision(ctx);
+            if (this.collision == 1) {
+                this.xPos -= this.xVel;
+                this.collision = 0;
+            }
         }
-        else if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_RIGHT) && this.xPos !== 482) {
-            this.xPos += this.xVel;
-        }
-        if (this.keyboardListener.isKeyDown(KeyboardListener.ARROW_LEFT)) {
+        if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_LEFT)) {
             this.xPos -= this.xVel;
+            this.checkCollision(ctx);
+            if (this.collision == 1) {
+                this.xPos += this.xVel;
+                this.collision = 0;
+            }
         }
-        else if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_LEFT)) {
-            this.xPos -= this.xVel;
-        }
-        if (this.keyboardListener.isKeyDown(KeyboardListener.ARROW_UP)) {
+        if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_UP)) {
             this.yPos -= this.yVel;
+            this.checkCollision(ctx);
+            if (this.collision == 1) {
+                this.yPos += this.yVel;
+                this.collision = 0;
+            }
         }
-        else if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_UP)) {
-            this.yPos -= this.yVel;
-        }
-        if (this.keyboardListener.isKeyDown(KeyboardListener.ARROW_DOWN)) {
+        if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_DOWN)) {
             this.yPos += this.yVel;
-        }
-        else if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_DOWN)) {
-            this.yPos += this.yVel;
-        }
-        if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_ESC)) {
-            window.location.replace("../scorescreen.html");
+            this.checkCollision(ctx);
+            if (this.collision == 1) {
+                this.yPos -= this.yVel;
+                this.collision = 0;
+            }
         }
     }
     draw(ctx) {
@@ -132,6 +123,90 @@ class Player {
         ctx.rect(this.xPos, this.yPos, x, y);
         ctx.closePath();
         ctx.fill();
+        this.coins(ctx);
+        this.endCheck();
+    }
+    checkCollision(ctx) {
+        let imgd = ctx.getImageData(this.xPos, this.yPos, 15, 15);
+        let pix = imgd.data;
+        for (let i = 0; i < pix.length; i += 4) {
+            if (pix[i] == 0) {
+                this.collision = 1;
+            }
+        }
+    }
+    rect(x, y, w, h, ctx) {
+        ctx.beginPath();
+        ctx.rect(x, y, w, h);
+        ctx.closePath();
+        ctx.fill();
+    }
+    coins(ctx) {
+        let x = [];
+        for (let i = 0; i < this.coin.length; i += 2) {
+            x.push(this.coin[i]);
+        }
+        let y = [];
+        for (let i = 1; i < this.coin.length; i += 2) {
+            y.push(this.coin[i]);
+        }
+        ctx.fillStyle = 'gold';
+        for (let i = 0; i < x.length; i++) {
+            ctx.beginPath();
+            ctx.rect(x[i], y[i], 15, 15);
+            ctx.closePath();
+            ctx.fill();
+        }
+        for (let i = 0; i < x.length; i++) {
+            if (this.xPos >= x[i] - 8 && this.xPos <= x[i] + 8 && this.yPos >= y[i] - 8 && this.yPos <= y[i] + 8) {
+                console.log(this.coin);
+                let indexX = this.coin.indexOf(x[i]);
+                let indexY = indexX + 1;
+                if (indexX % 2 !== 0 && indexX !== 0) {
+                    indexX = this.coin.indexOf(x[i], x[i] + 1);
+                    indexY = indexX + 1;
+                    console.log('testest');
+                }
+                console.log(indexX, indexY);
+                this.coin.splice(indexX, 1);
+                this.coin.splice(indexY - 1, 1);
+                ctx.clearRect(x[i], y[i], 15, 15);
+                ctx.fillStyle = 'purple';
+                this.rect(x[i], y[i], 15, 15, ctx);
+                console.log(this.coin);
+                this.coinCounter += 1;
+                document.getElementById('question').innerHTML = `Vraag: y/n?`;
+                document.getElementById('answers').innerHTML = `
+                <button id="option1">Y</button>
+                <button id="option2">N</button>`;
+                document.getElementById('option1').onclick = function () {
+                };
+            }
+        }
+        document.getElementById('Score').innerHTML = `Coins: ${this.coinCounter}`;
+    }
+    endCheck() {
+        if (this.yPos >= 156 && this.yPos <= 160 && this.xPos == 647) {
+            window.location.href = '../maze.html';
+        }
+    }
+    rand(min, max, step) {
+        var delta, range, rand;
+        if (arguments.length < 2) {
+            max = min;
+            min = 0;
+        }
+        if (!step) {
+            step = 1;
+        }
+        delta = max - min;
+        range = delta / step;
+        rand = Math.random();
+        rand *= range;
+        rand = Math.floor(rand);
+        rand *= step;
+        rand += min;
+        return rand;
     }
 }
 //# sourceMappingURL=app.js.map
